88972a1ccfffdd88d3d0a96c696fe519d262e80b
ilshookim <36989860+ilshookim@users.noreply.github.com>
2022-05-15 00:53:11 +0900

    * 수정/package.json: wscmd.js 이름으로 변경 * 추가/wscmd-zip.sh: npm run build-all 실행을 하고, 발생을 위해 ./wscmd-zip.sh 실행을 하면 wscmd-macos.zip, wscmd-linux.zip, wscmd-win.zip 압축파일을 생성

---
+++ wscmd-zip.sh
@@ -0,0 +1,11 @@
+cp wscmd-macos wscmd
+zip wscmd-macos.zip wscmd
+rm wscmd
+
+cp wscmd-linux wscmd
+zip wscmd-linux.zip wscmd
+rm wscmd
+
+cp wscmd-win.exe wscmd.exe
+zip wscmd-win.zip wscmd.exe
+rm wscmd.exe
